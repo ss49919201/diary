e69da4bc96d21cebbb917d2272fa95e91b7b065e
ss49919201 <sakashin10291029@gmail.com>
2023-12-02 09:42:17 +0900

    feat(domain): user constructor

--- user/domain/entity/user/user.go
+++ user/domain/entity/user/user.go
@@ -4,16 +4,24 @@ type User struct {
 	id UserID
 }
 
-type UserID struct {
-	value uint
+func NewUser(id UserID) User {
+	return User{id}
 }
 
-func NewUserID(v uint) UserID {
+func (u *User) UserID() UserID {
+	return u.UserID()
+}
+
+type UserID struct {
+	value string
+}
+
+func NewUserID(v string) UserID {
 	return UserID{
 		value: v,
 	}
 }
 
-func (u *UserID) Value() uint {
+func (u *UserID) Value() string {
 	return u.value
 }
