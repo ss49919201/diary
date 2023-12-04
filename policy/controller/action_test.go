13971aff68c7f5d9073407ef2126a1bf791e6213
ss49919201 <sakashin10291029@gmail.com>
2023-12-05 08:40:33 +0900

    refactor: unuse table driven test

--- policy/controller/action_test.go
+++ policy/controller/action_test.go
@@ -9,43 +9,24 @@ import (
 )
 
 func TestGetAction(t *testing.T) {
-	var rw *httptest.ResponseRecorder
+	// setup
+	rw := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(rw)
+	c.Request = httptest.NewRequest("GET", "/", httptest.NewRecorder().Body)
+	values := c.Request.URL.Query()
+	values.Set("user_id", "1")
+	values.Set("resource", "users")
+	values.Set("method", "read")
+	c.Request.URL.RawQuery = values.Encode()
 
-	type args struct {
-		c *gin.Context
+	// act
+	GetAction(c)
+
+	// assert
+	if rw.Result().StatusCode != http.StatusOK {
+		t.Errorf("expect: 200, actual: %d", rw.Result().StatusCode)
 	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			"success",
-			struct {
-				c *gin.Context
-			}{
-				func() *gin.Context {
-					rw = httptest.NewRecorder()
-					c, _ := gin.CreateTestContext(rw)
-					c.Request = httptest.NewRequest("GET", "/", httptest.NewRecorder().Body)
-					values := c.Request.URL.Query()
-					values.Set("user_id", "1")
-					values.Set("resource", "users")
-					values.Set("method", "read")
-					c.Request.URL.RawQuery = values.Encode()
-					return c
-				}(),
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			GetAction(tt.args.c)
-			if tt.args.c.Writer.Status() != http.StatusOK {
-				t.Errorf("expect: 200, actual: %d", tt.args.c.Writer.Status())
-			}
-			if rw.Body.String() != `{"allowed":true}` {
-				t.Errorf(`expect: {"allowed":true"}, actual: %s`, rw.Body.String())
-			}
-		})
+	if rw.Body.String() != `{"allowed":true}` {
+		t.Errorf(`expect: {"allowed":true"}, actual: %s`, rw.Body.String())
 	}
 }
