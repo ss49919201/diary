32e39ec9116184af38b0a3cb2d7211cd75b9cff8
ss49919201 <sakashin10291029@gmail.com>
2023-12-05 22:15:49 +0900

    feat: register user workflow

--- user/usecase/registeruser/registeruser.go
+++ user/usecase/registeruser/registeruser.go
@@ -2,6 +2,7 @@ package registeruser
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/ss49919201/diary/id"
@@ -11,7 +12,10 @@ import (
 
 type Input struct{}
 
-type Output struct{}
+type Output struct {
+	UserID       string
+	RegisteredAt time.Time
+}
 
 type Usecase interface {
 	Run(Input) (Output, error)
@@ -28,19 +32,71 @@ type usecase struct {
 }
 
 func (u *usecase) Run(in Input) (Output, error) {
-	out := Output{}
-	newUser := user.NewUser(user.NewUserID(u.generateID()))
-	ok, err := u.existsRegisterUserEvent(newUser.UserID())
+	w := new(workflow).
+		newUser(u.generateID).
+		registerUser(u.existsRegisterUserEvent, u.addRegisterUserEvent).
+		toOutput()
+	return w.output, w.err
+}
+
+type workflow struct {
+	err    error
+	u      user.User
+	re     registeruserevent.RegisterUserEvent
+	output Output
+}
+
+func (w *workflow) addError(err error) *workflow {
+	if err == nil {
+		return w
+	}
+
+	if w.err == nil {
+		w.err = err
+	} else {
+		w.err = fmt.Errorf("%v; %w", w.err, err)
+	}
+
+	return w
+}
+
+func (w *workflow) newUser(generateID id.Generate) *workflow {
+	w.u = user.NewUser(user.NewUserID(generateID()))
+	return w
+}
+
+func (w *workflow) registerUser(
+	existsRegisterUserEvent ExistsRegisterUserEvent,
+	addRegisterUserEvent AddRegisterUserEvent,
+) *workflow {
+	ok, err := existsRegisterUserEvent(w.u.UserID())
 	if err != nil {
-		return out, err
+		w.addError(err)
+		return w
 	}
 	if ok {
-		return out, errors.New("already exists")
+		w.addError(errors.New("already exists"))
+		return w
 	}
-	event := registeruserevent.NewRegisterUserEvent(newUser, time.Now())
-	_, err = u.addRegisterUserEvent(event)
+
+	event := registeruserevent.NewRegisterUserEvent(w.u, time.Now())
+	event, err = addRegisterUserEvent(event)
 	if err != nil {
-		return out, err
+		w.addError(err)
+		return w
 	}
-	return out, nil
+	w.re = event
+
+	return w
+}
+
+func (w *workflow) toOutput() *workflow {
+	w.output = Output{
+		UserID: func() string {
+			uid := w.re.UserID()
+			return uid.Value()
+		}(),
+		RegisteredAt: w.re.RegisteredAt(),
+	}
+	return w
 }
